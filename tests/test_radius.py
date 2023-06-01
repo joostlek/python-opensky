7604956ef5de5632b9f70ac04426065afe5f7166
OzGav <gavnosp@hotmail.com>
2023-06-01 21:34:48 +1000

    Improve bounding box calculations (#35)
    Co-authored-by: Joostlek <joostlek@outlook.com>

--- tests/test_radius.py
+++ tests/test_radius.py
@@ -25,6 +25,17 @@ PRECISION = 0.01
             ),
         ),
         (
+            45.0,
+            0.0,
+            111120,
+            BoundingBox(
+                min_latitude=44.0,
+                max_latitude=46.0,
+                min_longitude=-1.4,
+                max_longitude=1.438,
+            ),
+        ),
+        (
             360.0,
             0.0,
             111120,
@@ -77,11 +88,3 @@ async def test_calculating_bounding_box(
         bounding_box.max_longitude,
         PRECISION,
     )
-
-
-async def test_calculating_direction() -> None:
-    """Test calculating direction."""
-    second_point = OpenSky.calculate_point(0.0, 0.0, 25000.0, -180)
-    assert second_point == (-0.22609235747829648, 2.7503115231199028e-17)
-    second_point = OpenSky.calculate_point(0.0, 0.0, 25000.0, 361)
-    assert second_point == (0.22605792234324162, 0.003919461063277522)
