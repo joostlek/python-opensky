dfb0a49820a244b08ce51d2b747a5227dd32047b
Joost Lekkerkerker <joostlek@outlook.com>
2023-10-21 16:12:33 +0200

    Use syrupy (#167)

--- tests/test_states.py
+++ tests/test_states.py
@@ -6,14 +6,13 @@ import pytest
 from aiohttp import BasicAuth, ClientError
 from aiohttp.web_request import BaseRequest
 from aresponses import Response, ResponsesMockServer
+from syrupy.assertion import SnapshotAssertion
 
 from python_opensky import (
-    AircraftCategory,
     BoundingBox,
     OpenSky,
     OpenSkyConnectionError,
     OpenSkyError,
-    PositionSource,
     StatesResponse,
 )
 from python_opensky.exceptions import OpenSkyUnauthenticatedError
@@ -25,6 +24,7 @@ OPENSKY_URL = "opensky-network.org"
 
 async def test_states(
     aresponses: ResponsesMockServer,
+    snapshot: SnapshotAssertion,
 ) -> None:
     """Test retrieving states."""
     aresponses.add(
@@ -40,27 +40,7 @@ async def test_states(
     async with aiohttp.ClientSession() as session:
         opensky = OpenSky(session=session)
         response: StatesResponse = await opensky.get_states()
-        assert len(response.states) == 4
-        assert response.time == 1683488744
-        first_aircraft = response.states[0]
-        assert first_aircraft.icao24 == "ab1644"
-        assert first_aircraft.callsign == "UAL421  "
-        assert first_aircraft.origin_country == "United States"
-        assert first_aircraft.time_position == 1683488743
-        assert first_aircraft.last_contact == 1683488743
-        assert first_aircraft.longitude == -71.1656
-        assert first_aircraft.latitude == 42.5372
-        assert first_aircraft.barometric_altitude == 2217.42
-        assert not first_aircraft.on_ground
-        assert first_aircraft.velocity == 137.8
-        assert first_aircraft.true_track == 342.17
-        assert first_aircraft.vertical_rate == 13
-        assert first_aircraft.sensors is None
-        assert first_aircraft.geo_altitude == 2194.56
-        assert first_aircraft.transponder_code is None
-        assert not first_aircraft.special_purpose_indicator
-        assert first_aircraft.position_source == PositionSource.ADSB
-        assert first_aircraft.category == AircraftCategory.LARGE
+        assert response.model_dump() == snapshot
         await opensky.close()
 
 
